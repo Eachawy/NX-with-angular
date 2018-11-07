07b3fdfb900eba212e2ea177f465536179380680
mUhammad <Eachawy@gmail.com>
2018-11-07 13:34:40 +0200

    another modification

--- libs/apilib/src/lib/apilib.services.ts
+++ libs/apilib/src/lib/apilib.services.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class apilib {
   hello() {
-    return 'Services is Updated';
+    return 'Services is Updated new test';
   }
 }
